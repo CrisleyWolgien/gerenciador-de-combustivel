3b2ecaa8f48dfbd09b1b70459a0d05cfcabf5b8e
crisleywolgien <crisleyws@gmail.com>
2025-09-11 23:08:54 -0300

    ✨ feat: implementar cadastro de combustivel

--- gerenciador_combustivel/src/gerenciador_combustivel/routers/SupplyRouter.py
+++ gerenciador_combustivel/src/gerenciador_combustivel/routers/SupplyRouter.py
@@ -1,3 +1,24 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import Session
 from uuid import UUID
+
+from core.db import get_session
+from core.security import get_current_user
+from models.user import Users
+from schemas.schemas_supply import createSupply, readSupply
+from models.supply import Supply
+from repositories import Supply_crud
+
+router = APIRouter(prefix="/supply", tags=["Supply"])
+
+@router.post("/", status_code=201)
+def create_new_supply(
+    supply_in: createSupply,
+    vehicle_id: UUID,
+    session: Session = Depends(get_session),
+    current_user: Users = Depends(get_current_user)
+):
+    if current_user.id is None:
+        raise ValueError("Authenticated user does not have a valid ID.")
+    Supply_crud.create_supply(session, supply=supply_in, user_id=current_user.id, vehicle_id=vehicle_id)
+    return {"Usuario": get_current_user.name, "veiculo": vehicle_id, "Litros": supply_in.liters, "kilometragem": supply_in.km}
--- gerenciador_combustivel/src/gerenciador_combustivel/services/supply_service.py
+++
@@ -1,6 +0,0 @@
-from sqlmodel import Session
-from fastapi import HTTPException
-
-from repositories import Supply_crud
-from models.supply import Supply
-from schemas.schemas_supply import createSupply, readSupply
